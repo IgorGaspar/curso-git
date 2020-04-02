0ed43ba288e035b8841fad7ad2a9090d27ed35c0
Igor Gaspar <igor.elvis@hotmail.com>
2020-04-02 16:24:59 -0300

    Acrescentada a mensagem de despedida

--- ConsoleApp9/Program.cs
+++ ConsoleApp9/Program.cs
@@ -8,6 +8,7 @@ namespace ConsoleApp9 {
             Console.WriteLine("Olá mundo!");
             Console.WriteLine("Como você está hoje?");
             Console.WriteLine("Tenha um bom dia!");
+            Console.WriteLine("Tchau.");
 
             Console.ReadLine();
 
