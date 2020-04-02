3d34bb8af1a9086d32c1f63b0fbb70e3a1968361
Igor Gaspar <igor.elvis@hotmail.com>
2020-04-02 16:20:31 -0300

    Acrescentada a mensagem desejando um bom dia

--- ConsoleApp9/Program.cs
+++ ConsoleApp9/Program.cs
@@ -6,8 +6,8 @@ namespace ConsoleApp9 {
         static void Main(string[] args) {
 
             Console.WriteLine("Olá mundo!");
-            Console.WriteLine("Bom dia!");
             Console.WriteLine("Como você está hoje?");
+            Console.WriteLine("Tenha um bom dia!");
 
             Console.ReadLine();
 
